f32d4beab8eca3328862654d20060f9dc1a5388d
gabrielalmeidaecks <128308662+gabrielalmeidaecks@users.noreply.github.com>
2024-10-23 19:42:24 -0300

    aula 23/10

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -4,5 +4,10 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados)
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que  do total de ${dados.total_pessoas_mundo} de pessoas no mundo,  ${dados.total_pessoas_que_necessitam_transporte_para_trabalho} são trabalhadores e aproximadamente 66% necessitam de transporte para ir ao trabalho?  E o tempo médio gasto para o deslocamento ao trabalho é de menos de ${dados.tempo_medio_deslocamento_para_trabalho} por dia? Isso significa que eles destinam 3,75% do dia somente em deslocamento`
+    container.appendChild(paragrafo);
 }
 vizualizarInformacoesGlobais()
+
