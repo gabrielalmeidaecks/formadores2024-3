9dce668eb43535a9635dbb91de5a16353f15df02
gabrielalmeidaecks <128308662+gabrielalmeidaecks@users.noreply.github.com>
2024-10-30 19:15:47 -0300

    aula 05 2

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
     console.log(dados)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que  do total de ${dados.total_pessoas_mundo} de pessoas no mundo,  ${dados.total_pessoas_que_necessitam_transporte_para_trabalho} são trabalhadores e aproximadamente 66% necessitam de transporte para ir ao trabalho?  E o tempo médio gasto para o deslocamento ao trabalho é de menos de ${dados.tempo_medio_deslocamento_para_trabalho} por dia? Isso significa que eles destinam 3,75% do dia somente em deslocamento`
+    paragrafo.innerHTML = `Você sabia que  do total de <span>${dados.total_pessoas_mundo}</span> de pessoas no mundo,  <span>${dados.total_pessoas_que_necessitam_transporte_para_trabalho}</span> são trabalhadores e aproximadamente 66% necessitam de transporte para ir ao trabalho?  E o tempo médio gasto para o deslocamento ao trabalho é de menos de <span>${dados.tempo_medio_deslocamento_para_trabalho}</span> por dia? Isso significa que eles destinam 3,75% do dia somente em deslocamento`
     container.appendChild(paragrafo);
 }
 vizualizarInformacoesGlobais()
