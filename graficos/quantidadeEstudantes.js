d1f9eb97276ee3155fe85bbea6a96b318f7f6168
gabrielalmeidaecks <128308662+gabrielalmeidaecks@users.noreply.github.com>
2024-11-13 19:10:10 -0300

    agora

--- graficos/quantidadeEstudantes.js
+++ graficos/quantidadeEstudantes.js
@@ -1,5 +1,5 @@
 async function quantidadeEstudantes() {
-'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-etapas-de-ensino.json'
+  https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-etapas-de-ensino.json
 const res = await fetch(url)
     const dados = await res.json()
     const nomeDasEscolas = Object.keys(dados)
