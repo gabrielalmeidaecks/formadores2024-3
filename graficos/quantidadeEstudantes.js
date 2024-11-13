4282e8b6e33954b5672e38c94b9736ca3e4535e1
gabrielalmeidaecks <128308662+gabrielalmeidaecks@users.noreply.github.com>
2024-11-13 19:31:22 -0300

    Update quantidadeEstudantes.js

--- graficos/quantidadeEstudantes.js
+++ graficos/quantidadeEstudantes.js
@@ -17,22 +17,3 @@ const res = await fetch(url)
     Plotly.newPlot(grafico, data)
 }
 quantidadeEstudantes()
-async function quantidadeEstudantes() {
-  const url = 'https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/educacao/educacao-etapas-de-ensino.json'
-  const res = await fetch(url)
-  const dados = await res.json()
-  const nomeDasEscolas = Object.keys(dados)
-  const quantidadeEstudantes = Object.values(dados)
-  const data = [
-      {
-        x: nomeDasEscolas,
-        y: quantidadeEstudantes,
-        type: 'bar'
-      }
-    ]
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
-}
-quantidadeEstudantes()
